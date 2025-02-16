a66552b52f2f8429f62f74ed58e877714cb65ed0
Alexander Mandrov <otvet-1995@mail.ru>
2025-02-16 11:21:55 +0200

    feat(#ia4): response with 200

--- app/server.go
+++ app/server.go
@@ -19,11 +19,11 @@ func main() {
 		os.Exit(1)
 	}
 	
-	_, err = listener.Accept()
+	conn, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	fmt.Print("HTTP/1.1 200 OK\r\n\r\n")
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
