0d0dea0fcad2d33114aad3bc84b97455b1dcac1e
Alexander Mandrov <otvet-1995@mail.ru>
2025-02-16 11:31:14 +0200

    feat(#ih0): read incomming connection request

--- app/server.go
+++ app/server.go
@@ -25,5 +25,12 @@ func main() {
 		os.Exit(1)
 	}
 
+	n, err := conn.Read([]byte(make([]byte, 1024)))
+	if err != nil {
+    fmt.Println("Error reading from connection: ", err.Error())
+    os.Exit(1)
+  }
+
+	fmt.Printf("Received %d\n", n)
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
